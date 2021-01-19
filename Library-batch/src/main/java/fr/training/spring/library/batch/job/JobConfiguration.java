bcbc1acf8d6f60f947afbe29ff7756a68722212b
Gwerrhod <abaillot.bnpparibas@gmail.com>
2021-01-19 11:50:41 +0100

    swagger info début batch

--- Library-batch/src/main/java/fr/training/spring/library/batch/job/JobConfiguration.java
+++ Library-batch/src/main/java/fr/training/spring/library/batch/job/JobConfiguration.java
@@ -7,20 +7,40 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import fr.training.spring.library.batch.common.FullReportListener;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 @Configuration
 public class JobConfiguration {
 
 	private static final Logger logger = LoggerFactory.getLogger(JobConfiguration.class);
+
+	private String[] Headers = {"id", "type", "addressNumber", "addressStreet",  "addressPostalCode",
+			"addressCity","directorSurname","directorName"};
 
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
@@ -31,9 +51,12 @@ public class JobConfiguration {
 	@Autowired
 	private FullReportListener jobListener;
 
+	@Autowired
+	public DataSource dataSource;
+
 	@Bean
 	public Job exportJob(final Step step1) {
-		return jobBuilderFactory.get("job") //
+		return jobBuilderFactory.get("jobExport") //
 				.incrementer(new RunIdIncrementer()) // job can be launched as many times as desired
 				.start(step1) //
 				.listener(jobListener) //
@@ -51,6 +74,75 @@ public class JobConfiguration {
 						return RepeatStatus.FINISHED;
 					}
 				}).build();
+	}
+
+	/**
+	 * ItemReader is an abstract representation of how data is provided as input to
+	 * a Step. When the inputs are exhausted, the ItemReader returns null.
+	 */
+	@Bean
+	public JdbcCursorItemReader<LibraryDTO> exportReader() {
+		final JdbcCursorItemReader<LibraryDTO> reader = new JdbcCursorItemReader<LibraryDTO>();
+		reader.setDataSource(dataSource);
+		reader.setSql("SELECT id, type, " +
+				"addressNumber, addressStreet,  addressPostalCode" +
+				"addressCity,directorSurname,directorName"+
+				"FROM Library");
+		reader.setRowMapper(new LibraryRowMapper());
+
+		return reader;
+	}
+
+	/**
+	 * RowMapper used to map resultset to OrderDto
+	 */
+	public class LibraryRowMapper implements RowMapper<LibraryDTO> {
+
+		@Override
+		public LibraryDTO mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+			final LibraryDTO libraryDto = new LibraryDTO();
+			libraryDto.setId(rs.getString("id"));
+			libraryDto.setType(rs.getString("type"));
+			libraryDto.setAddressNumber(rs.getString("addressNumber"));
+			libraryDto.setAddressStreet(rs.getString("addressStreet"));
+			libraryDto.setAddressPostalCode(rs.getString("addressPostalCode"));
+			libraryDto.setAddressCity(rs.getString("addressCity"));
+			libraryDto.setDirectorSurname(rs.getString("directorSurname"));
+			libraryDto.setDirectorName(rs.getString("directorName"));
+			return libraryDto;
+		}
+	}
+
+	@Bean
+	public ItemProcessor<LibraryDTO, LibraryDTO> exportProcessor() {
+		return new ItemProcessor<LibraryDTO, LibraryDTO>() {
+
+			@Override
+			public LibraryDTO process(final LibraryDTO library) throws Exception {
+				logger.info("Processing {}", library);
+				return library;
+			}
+		};
+	}
+
+	@StepScope // Mandatory for using jobParameters
+	@Bean
+	public FlatFileItemWriter<LibraryDTO> exportWriter(@Value("#{jobParameters['output-file']}") final String outputFile) {
+		final FlatFileItemWriter<LibraryDTO> writer = new FlatFileItemWriter<LibraryDTO>();
+		writer.setResource(new FileSystemResource(outputFile));
+		final DelimitedLineAggregator<LibraryDTO> lineAggregator = new DelimitedLineAggregator<LibraryDTO>();
+		final BeanWrapperFieldExtractor<LibraryDTO> fieldExtractor = new BeanWrapperFieldExtractor<LibraryDTO>();
+		fieldExtractor.setNames(Headers);
+		lineAggregator.setFieldExtractor(fieldExtractor);
+		lineAggregator.setDelimiter(";");
+		writer.setLineAggregator(lineAggregator);
+		writer.setHeaderCallback(new FlatFileHeaderCallback() {
+			@Override
+			public void writeHeader(final Writer writer) throws IOException {
+				writer.write(String.valueOf(Headers));
+			}
+		});
+		return writer;
 	}
 
 }
---
+++ Library-batch/src/main/java/fr/training/spring/library/batch/job/LibraryDTO.java
@@ -0,0 +1,85 @@
+package fr.training.spring.library.batch.job;
+
+public class LibraryDTO {
+
+
+    private String id;
+
+    private String type;
+
+    private String addressNumber;
+
+    private String addressStreet;
+
+    private String addressPostalCode;
+
+    private String addressCity;
+
+    private String directorSurname;
+
+    private String directorName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+    public void setAddressStreet(String addressStreet) {
+        this.addressStreet = addressStreet;
+    }
+
+    public String getAddressPostalCode() {
+        return addressPostalCode;
+    }
+
+    public void setAddressPostalCode(String addressPostalCode) {
+        this.addressPostalCode = addressPostalCode;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public String getDirectorSurname() {
+        return directorSurname;
+    }
+
+    public void setDirectorSurname(String directorSurname) {
+        this.directorSurname = directorSurname;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+}
